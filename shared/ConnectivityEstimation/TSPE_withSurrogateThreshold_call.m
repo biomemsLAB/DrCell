5f49ca636178743dd398d8556b4b58f7954ba755
Broxy <67224622+BranchBroxy@users.noreply.github.com>
2022-03-01 14:57:29 +0100

    fix spelling in TSPE_withSurrogateThreshold_call.m

--- shared/ConnectivityEstimation/TSPE_withSurrogateThreshold_call.m
+++ shared/ConnectivityEstimation/TSPE_withSurrogateThreshold_call.m
@@ -6,7 +6,7 @@ sdf=TS_M2sdf(TS,rec_dur); % convert format of time stamps like needed by TSPE
 CM=TSPE_withSurrogateThreshold(sdf,[],[],[],[],[],[],[],[],flag_waitbar); % CMres: NxN matrix where N(i, j) is the total spiking probability edges (TSPE) i->j
 CM = CM - diag(diag(CM)); % set diagonal to zero (=deleting self loops)
 
-[CM_exh,CM_inh]=seperateConnectivityMatrixToInhExh(CM);
+[CM_exh,CM_inh]=seperateConnectivityMatrixToInhExc(CM);
 
 CM(isnan(CM))=0;
 CM_exh(isnan(CM_exh))=0;
