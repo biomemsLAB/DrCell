63d7b72881764be59df3019300d5800e693f7e0c
Manuel Ciba <manuel.ciba.hab@gmail.com>
2026-05-20 21:17:20 +0200

    Refactor plate ID extraction in axion_spk2TS.m
    Updated the method of extracting plate ID from file info.

--- shared/DataConversion/axion_spk2TS.m
+++ shared/DataConversion/axion_spk2TS.m
@@ -103,7 +103,11 @@ function axion_spk2TS(file_path)
                 SPIKEZ = createStructure_SPIKEZ(TS,AMP,SaRa,rec_dur,fileinfo,nr_channel,Time,Date,EL_NAMES,EL_NUMS);
                 wellName = [wellRowNames{i} wellColNames{j}];
                 [root_path, file_name, ext] = fileparts(file_path);
-                plate_id = regexp(file_name, 'Plate\d+', 'match', 'once');
+
+                % get plate id
+                tokens = regexp(fileinfo, 'Plate Serial Number,(\d+\-\d+)', 'tokens');
+                plate_id = tokens{1}{1};
+                
                 if ~isempty(plate_id)
                     folder = [root_path filesep 'TS' filesep wellName '_' plate_id];
                 else
